47245c3efdbf924b2c569a0feb75790f3e39a43e
Rachel Shorey <rshorey@gmail.com>
2016-02-28 21:59:54 -0500

    show page working but still needs some formatting

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 
 #pull this off your google sheet
 sheet_id = '125NM05dqHKy3-97GkieVwn4V5Iy0Vj-w7erh9tAJxlA'
@@ -10,18 +11,18 @@ require_approval = False
 #what do you want the fields to be called?
 #the cannonical names are keys, the names you want to display are values
 #don't remove any keys, things will crash
-fieldnames = {'Name':'Name',
-        'Description':'Description',
-        'Category':'Category',
-        'Address1':'Address line 1',
-        'Address2':'Address line 2',
-        'City':'City',
-        'State':'State',
-        'Zip':'Zipcode',
-        'Phone':'Phone number',
-        'Website':'Website',
-        'Email':'Email Address'
-        }
+fieldnames = OrderedDict([('Name','Name'),
+        ('Description','Description'),
+        ('Category','Category'),
+        ('Address1','Address line 1'),
+        ('Address2','Address line 2'),
+        ('City','City'),
+        ('State','State'),
+        ('Zip','Zipcode'),
+        ('Phone','Phone number'),
+        ('Website','Website'),
+        ('Email','Email Address')
+        ])
 
 #what categories are allowed? Put in as many as you want but
 #they'll display in a menu so if you do tons it'll look bad
